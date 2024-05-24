9fb294517ffd724762fb8f2d36c5c88b358cf50e
Gerl4ch <leonardohg2005@gmail.com>
2024-05-23 21:45:18 -0300

    Top Menu

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,15 +7,18 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavView;
+    MaterialToolbar idTopBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,39 @@ public class MainActivity extends AppCompatActivity {
         });
         //Apresentando o XML para o JAVA
         bottomNavView = findViewById(R.id.bottomNavView);
+        idTopBar = findViewById(R.id.idTopBar);
+
+        //Criando o evento de clique na TopBar
+        idTopBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.mFav) {
+                    Toast.makeText(getApplicationContext(),
+                            "Favoritei", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mExplore) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Explorar", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mSearch) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei em Pesquisar", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mCloud) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei na Nuvem", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mLocation) {
+                    Toast.makeText(getApplicationContext(),
+                            "Cliquei na Localização", Toast.LENGTH_SHORT).show();
+                }
+                if (item.getItemId() == R.id.mShare) {
+                    Toast.makeText(getApplicationContext(),
+                            "CLiquei em Compartilhar", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
 
         //Criando evento de click nos itens de menu
         bottomNavView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
